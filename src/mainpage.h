81525615f193152d66c49f4237bf346844a17dcf
burnett <>
2006-11-28 03:03:12 +0000

    initial import

---
+++ src/mainpage.h
@@ -0,0 +1,19 @@
+// $Header: /nfs/slac/g/glast/ground/cvs/users/burnett/embed_python/src/mainpage.h,v 1.1 2006/11/28 03:00:42 burnett Exp $
+// Mainpage for doxygen
+
+/*! \mainpage package embed_python
+
+   \authors  Toby Burnett
+
+
+  This package builds a library to facilitate embedding python.
+
+
+    <hr>
+  \section notes release notes
+  release.notes
+  \section requirements requirements
+  \include requirements
+
+*/
+
---
+++ src/test/runPy.cxx
@@ -0,0 +1,29 @@
+/** @file runPy.cxx
+*/
+
+#include "embed_python/Module.h"
+#include <iostream>
+#include <stdexcept>
+
+
+int
+main(int argc, char *argv[])
+{
+    return embed_python::Module::test(argc, argv, "Test");
+}
+
+/** test with the following python code:
+@verbatim
+
+
+def setup():
+    print "seting up a dictionary..."
+    dict = {'x':99}
+    dict['y']=101
+    dict['z']='testing'
+    return dict
+       
+    
+    @endverbatim
+
+*/
