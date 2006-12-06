c56230d1851c921412d3250ebdb081ddd49cc975
burnett <>
2006-12-06 16:15:15 +0000

    add recognition of compound name, lists of numbers.

--- python/Test.py
+++ python/Test.py
@@ -2,13 +2,15 @@
 print "this is Test.py"
 import math # see if this works
 
-# make a list
+# make a list of strings
 names = ['one', 'two', 'three']
+
+# a list of values
+values = range(5)
 
 #define a few attributes
 x=99.
 z = 'test string'
-pi = math.pi
 
 print 'directory ', dir()
 print
