67d0ce001ee46e2fc0b6701640200c0ea209d35f
cvs2git <>
2007-02-03 16:14:03 +0000

    This commit was manufactured by cvs2svn to create branch 'ScienceToolsv8r2'.
    Sprout from THB 2007-02-03 16:13:59 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2007-02-03 16:13:55 UTC jchiang 'insert . into sys.path; tagging v1r1p1':
        cmt/requirements
        doc/release.notes
        embed_python/Module.h
        python/Test.py
        src/Module.cxx

--- python/Test.py
+++ python/Test.py
@@ -1,15 +1,26 @@
-
 print "this is Test.py"
 import math # see if this works
 
-# make a list
+# make a list of strings
 names = ['one', 'two', 'three']
+
+# a list of values
+values = range(5)
+
+def callable():
+    print "callable function was called!"
 
 #define a few attributes
 x=99.
 z = 'test string'
-pi = math.pi
 
+# make a class with attributes
+class A(object):
+    a = 99
+    b = -99
+    c ='a name'
+    
+    
 print 'directory ', dir()
 print
     
