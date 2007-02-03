e37c42f111bb46d94cafe545f5dd4e66cadcd033
cvs2git <>
2007-02-03 16:14:02 +0000

    This commit was manufactured by cvs2svn to create branch 'ScienceToolsv8r1'.
    Sprout from THB 2007-02-03 16:13:59 UTC jchiang 'AutoUpdate'
    Cherrypick from master 2007-02-03 16:13:55 UTC jchiang 'insert . into sys.path; tagging v1r1p1':
        cmt/requirements
        doc/release.notes
        embed_python/Module.h
        python/Test.py
        src/Module.cxx

--- embed_python/Module.h
+++ embed_python/Module.h
@@ -1,7 +1,7 @@
 /** @file Module.h
     @brief declare Module class
 
-    $Header: /nfs/slac/g/glast/ground/cvs/users/burnett/embed_python/embed_python/Module.h,v 1.2 2006/11/28 03:00:41 burnett Exp $
+    $Header: /nfs/slac/g/glast/ground/cvs/embed_python/embed_python/Module.h,v 1.7 2006/12/14 21:48:26 lsrea Exp $
 */
 #ifndef embed_python_Module_h
 #define embed_python_Module_h
@@ -24,24 +24,41 @@ namespace embed_python {
 */
 class Module {
 public:
-    /// ctor 
+
     /// @param path path to module. If empty, assume cwd
-    /// @param module name of module to load, without ".py".  Must be on pythonpath
+    /// @param module name of module to load, without ".py". 
+    /// @param python_dir Directory to be prepended to PYTHONPATH
     /// @param verbose [false] useful output if debugging?
-    Module(std::string path, std::string module, bool verbose=false);
+    Module(const std::string& path, const std::string& module, 
+           const std::string & python_dir="",
+           bool verbose=false);
+           
     ~Module();
 
     /// return a numeric type
-    double operator[](std::string key);
+    double operator[](const std::string& key);
 
-    /// set a numeric value
-    void getValue(std::string attribute, double & value);
+
+    /// set an int value
+    void getValue(const std::string& attribute, int & value);
+    /// set an int value with default
+    void getValue(const std::string& attribute, int & value, int default_value);
+
+    /// set a double value
+    void getValue(const std::string& attribute, double & value);
+    /// set a double value with default
+    void getValue(const std::string& attribute, double & value, double default_value);
 
     /// set a string value
-    void getValue(std::string attribute, std::string& value);
+    void getValue(const std::string& attribute, std::string& value);
+    /// set a string value with default
+    void getValue(const std::string& attribute, std::string& value, std::string default_value);
 
     /// set a list of strings
-    void getList(std::string listname, std::vector<std::string>& names);
+    void getList(const std::string& listname, std::vector<std::string>& names);
+
+    /// set a list of values
+    void getList(const std::string& listname, std::vector<double>& values);
 
     /// examine type of a PyObject
     std::string type(const PyObject* obj)const;
@@ -50,22 +67,28 @@ public:
     PyObject* call(PyObject* o, PyObject* args=0 )const;
 
     /// return the attribute -- exception if does not exist
-    PyObject * attribute(std::string name);
+    /// @param name the attribute name, perhaps compound
+    /// @param check if false, do not check for success, return null
+    PyObject * attribute(const std::string& name, bool check=true);
 
     /// access root, if path was defined
     std::string root()const{return m_root;}
 
     /// test program, to be called directly from a main.
-    static int test(int argc, char* argv[], std::string modulename);
+    static int test(int argc, char* argv[], const std::string& modulename);
+
 
 private:
 
     PyObject * m_module; ///< the module we manage
+    std::string m_moduleName; 
 
     bool m_verbose;
     bool verbose()const{return m_verbose;}
     void check_error(const std::string& text)const;
     std::string m_root; ///< python path
+
+   void insert_path(const std::string & dir);
 };
 
 }// namespace
