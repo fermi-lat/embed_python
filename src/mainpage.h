78f253f62d814311f40a59100a0ea6aecef446a8
burnett <>
2008-06-20 02:32:01 +0000

    support for a dictionary of floats --> map<string,douible>

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/users/burnett/embed_python/src/mainpage.h,v 1.1 2006/11/28 03:00:42 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/embed_python/src/mainpage.h,v 1.1.1.1 2006/11/28 03:03:12 burnett Exp $
 // Mainpage for doxygen
 
 /*! \mainpage package embed_python
@@ -7,6 +7,7 @@
 
 
   This package builds a library to facilitate embedding python.
+  All the work is done by the class embed_python::Module
 
 
     <hr>
