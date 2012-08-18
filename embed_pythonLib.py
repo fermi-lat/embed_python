cb2f3ac3f561cc6bf2e00ba76efd3b6c47aac4b3
jrb <>
2012-08-18 00:25:09 +0000

    support no-install headers on Windows

--- embed_pythonLib.py
+++ embed_pythonLib.py
@@ -1,10 +1,13 @@
-#$Id: embed_pythonLib.py,v 1.2 2008/02/26 03:06:47 glastrm Exp $
+#$Id: embed_pythonLib.py,v 1.3 2010/10/05 19:24:18 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['embed_python'])
 	if env['PLATFORM'] == 'posix':
             env.AppendUnique(LINKFLAGS	= ['-rdynamic'])	
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'embed_python') 
     env.Tool('addLibrary', library = env['pythonLibs'])
+    # No need for incsOnly section
 
 def exists(env):
     return 1
