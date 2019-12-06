075b0735a409f17e693bf9e06fb85b11a795a321
Tom Stephens <thomas.stephens@gmail.com>
2019-12-06 11:29:26 -0700

    Fixed tabs and indention issues

--- embed_pythonLib.py
+++ embed_pythonLib.py
@@ -2,10 +2,10 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['embed_python'])
-	if env['PLATFORM'] == 'posix':
-            env.AppendUnique(LINKFLAGS	= ['-rdynamic'])	
-        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
-            env.Tool('findPkgPath', package = 'embed_python') 
+    if env['PLATFORM'] == 'posix':
+        env.AppendUnique(LINKFLAGS	= ['-rdynamic'])	
+    if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+        env.Tool('findPkgPath', package = 'embed_python') 
     env.Tool('addLibrary', library = env['pythonLibs'])
     # No need for incsOnly section
 
